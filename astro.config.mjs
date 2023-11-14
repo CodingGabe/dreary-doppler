b93cc79e1a6bfd0f7623de1f99d711d39cdcecd2
Gabe Amaya <gabeamaya2@gmail.com>
2023-11-14 13:40:41 -0600

    fix nav, add mobile nav, fix mobile layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), astroImageTools]
+  site: 'https://gabes-astro-code-blog.netlify.app',
+  integrations: [tailwind(), mdx(), astroImageTools],
+  output: "server",
+  adapter: netlify()
 });
