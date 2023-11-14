d2fad35e938ccaf66776d768213cb6f198337a0e
Gabe Amaya <gabeamaya2@gmail.com>
2023-11-14 15:06:21 -0600

    remove netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 import mdx from "@astrojs/mdx";
-
 import netlify from "@astrojs/netlify/functions";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gabes-astro-code-blog.netlify.app',
   integrations: [tailwind(), mdx(), astroImageTools],
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
