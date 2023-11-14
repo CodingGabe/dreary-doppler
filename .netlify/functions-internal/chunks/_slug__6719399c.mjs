95f9dabece9a95783fff1c9f216fa743af324dfd
Gabe Amaya <gabeamaya2@gmail.com>
2023-11-14 14:02:03 -0600

    removed netflify

--- .netlify/functions-internal/chunks/_slug__6719399c.mjs
+++
@@ -1,9 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import './astro_923801e0.mjs';
-import 'clsx';
-import 'html-escaper';
-
-const page = () => import('./prerender_88c5e0f8.mjs');
-
-export { page };
